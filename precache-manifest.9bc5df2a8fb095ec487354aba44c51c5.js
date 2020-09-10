0eaf4c5c90f799c3b6830a220b75faea5ee3f85b
Julian Grande <juliangr@stud.ntnu.no>
2020-09-10 18:00:26 +0200

    Updates

---
+++ precache-manifest.9bc5df2a8fb095ec487354aba44c51c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf44025f22e1c8baf20c5a9ba9c44cf",
+    "url": "/Infoskjerm/index.html"
+  },
+  {
+    "revision": "2c0a367718fdb105b53a",
+    "url": "/Infoskjerm/static/css/main.30ea4fde.chunk.css"
+  },
+  {
+    "revision": "709d4e1fd7a281522997",
+    "url": "/Infoskjerm/static/js/2.33889596.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Infoskjerm/static/js/2.33889596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c0a367718fdb105b53a",
+    "url": "/Infoskjerm/static/js/main.35354327.chunk.js"
+  },
+  {
+    "revision": "ff7c7fb72a57287e703d",
+    "url": "/Infoskjerm/static/js/runtime-main.4fa9607a.js"
+  }
+]);
